a83041ccd72c20949e3e3c68dea33f58609abc0d
Nikita <54949225+NikitaPatel15@users.noreply.github.com>
2020-02-20 18:17:02 -0400

    "All features's UI"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Admin_schedule_details from './components/admin_schedule/admin_schedule_details';
-import User_schedule_view from './components/user_schedule/user_schedule_view';
-import Login from './components/Login/login';
+import Admin_schedule_display from './components/Schedule/admin_schedule_display';
+import User_schedule_display from './components/Schedule/user_schedule_display';
+import Login from './components/Profile Management/Login/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component{
@@ -11,8 +11,8 @@ class App extends Component{
 			<Router>
                 {/* <Route exact path='/' component={Home} /> */}
 				
-				<Route path='/admin_schedule_details' component={Admin_schedule_details} />
-				<Route path='/user_schedule_view' component={User_schedule_view} />
+				<Route path='/admin_schedule_display' component={Admin_schedule_display} />
+				<Route path='/user_schedule_display' component={User_schedule_display} />
 				<Route path='/login' component={Login} />
 			
 			</Router>
