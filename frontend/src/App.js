af3047986f73930aa2c38e4687ed7573079cb569
Nikita Vijaybhai Patel <nk996784@dal.ca>
2020-02-20 18:56:05 -0400

    Revert "Merge branch 'NikitaFeatures' into 'master'"
    This reverts merge request !17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Admin_schedule_display from './components/Schedule/admin_schedule_display';
-import User_schedule_display from './components/Schedule/user_schedule_display';
-import Login from './components/Profile Management/Login/login';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component{
-    render() {
-		return (
-			<Router>
-                <Route exact path='/' component={Home} />
-				
-				<Route path='/admin_schedule_display' component={Admin_schedule_display} />
-				<Route path='/user_schedule_display' component={User_schedule_display} />
-				<Route path='/login' component={Login} />
-			
-			</Router>
-		);
-	}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
