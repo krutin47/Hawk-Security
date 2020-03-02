de4abd8aa602a0a99fb9f2a7d5830a19f552b092
Nikita <54949225+NikitaPatel15@users.noreply.github.com>
2020-03-02 13:07:50 -0400

    UI Check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Availability_form from './components/Availability/availability_form';
 import Admin_schedule_form from './components/Schedule/admin_schedule_form';
-import Availability_display from './components/Availability/availability_dsiplay';
+import Availability_display from './components/Availability/availability_display';
 import Admin_schedule_display from './components/Schedule/admin_schedule_display';
 import User_schedule_display from './components/Schedule/user_schedule_display';
 import Login from './components/ProfileManagement/Login/login';
