eac457c1024718ea594b648fa4abac47e98dda5c
@bhanderi <milap@dal.ca>
2020-03-11 12:59:13 -0300

    job adding UI

--- frontend/src/components/Career/job_display.js
+++ frontend/src/components/Career/job_display.js
@@ -1,106 +1,27 @@
 import React from 'react';
 import '../../main.css';
 
-function availability_form() {
+function job_display() {
     return (
         <React.Fragment>
-            
-            <section class="pb70 sectionBox">
-                <div class="wrapper smallWrapper">
-                    <div class="text-center uppercase">
-                        <h3>Job</h3>
-                    </div>
-                    <div class="pt40">
-                        <form>
-                            <div>
-                                <div class="ib vt w50 mw100">
-                                    <div class="field">
-                                        <label for="firstname">First Name <sup>*</sup></label>
-                                        <input type="text" name="firstname" id="firstname" required></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w50 mw100">
-                                    <div class="field">
-                                        <label for="lastname">Last Name <sup>*</sup></label>
-                                        <input type="text" name="lastname" id="lastname" required></input>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="ib vt w50 mw100">
-                                    <div class="field">
-                                        <label for="emId">Employee Id <sup>*</sup></label>
-                                        <input type="text" name="emId" id="emId" required></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w50 mw100">
-                                    <div class="field">
-                                        <label for="efDate">Effective Date *</label>
-                                        <input type="text" name="efDate" id="efDate" required></input>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="monday">Monday</label>
-                                        <input type="text" name="monday" id="monday"></input>
-                                        <input type="text" name="monday" id="monday"></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="tuesday">Tuesday</label>
-                                        <input type="text" name="tuesday" id="tuesday"></input>
-                                        <input type="text" name="tuesday" id="tuesday"></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="wednesday">Wednesday</label>
-                                        <input type="text" name="wednesday" id="wednesday"></input>
-                                        <input type="text" name="wednesday" id="wednesday"></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="thursday">Thursday</label>
-                                        <input type="text" name="thursday" id="thursday"></input>
-                                        <input type="text" name="thursday" id="thursday"></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="friday">Friday</label>
-                                        <input type="text" name="friday" id="friday"></input>
-                                        <input type="text" name="friday" id="friday"></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="saturday">Saturday</label>
-                                        <input type="text" name="saturday" id="saturday"></input>
-                                        <input type="text" name="saturday" id="saturday"></input>
-                                    </div>
-                                </div>
-                                <div class="ib vt w14 tabw25 mw100">
-                                    <div class="field">
-                                        <label for="sunday">Sunday</label>
-                                        <input type="text" name="sunday" id="sunday"></input>
-                                        <input type="text" name="sunday" id="sunday"></input>
-                                    </div>
-                                </div>
-                                 <p><sup>*</sup>Required Fields.</p>
-                            </div>
-                            <div class="text-center pt30">
-                                <input type="submit" name="submit" value="submit"></input>
-                            </div>
-                        </form>
-                    </div>
+            {/* <div className="headerMain">
+            <div className="wrapper clearfix">
+                <div className="siteNavigation fr">
+                    <Link to="/job_display">JOB DISPLAY</Link>
                 </div>
-            </section>
+            </div>
+          </div> */}
+
+        
+            <div className="card">
+  <img src="/w3images/jeans3.jpg" alt="Denim Jeans"/>
+  <h1>Tailored Jeans</h1>
+  <p>$19.99</p>
+  <p>Some text about the jeans. Super slim and comfy lorem ipsum lorem jeansum. Lorem jeamsun denim lorem jeansum.</p>
+  <p><button>Add to Cart</button></p>
+</div>
         </React.Fragment>    
     )
 }
 
-export default availability_form
+export default job_display
