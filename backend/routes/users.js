e467a099c40ea4d975f663e679a706b63f28fc84
Nikita <54949225+NikitaPatel15@users.noreply.github.com>
2020-03-11 14:45:18 -0300

    mongodb atlas connections and location backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+
+let Location = require('../models/users.model.js'); 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+
+router.route('/').get((req, res) => {
+  Location.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+  const location_Id = req.body.Location_ID;
+  const name = req.body.Name;
+  const address = req.body.Address;
+
+  const newlocation = new Location({ Location_ID: location_Id, Name: name, Address: address });
+
+  newlocation.save()
+    .then(() => res.json('Location added!' + newlocation))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
